64d413e60cd82d77afdb65bd0272b33f5b782c38
Alberto Perdigão <alberto@PGE.CE.GOV.BR>
2022-01-06 17:51:43 -0300

    First commit, added REST API to access items, stores, and user authentication.

---
+++ python-flask-sqlalchemy/README.md
@@ -0,0 +1,48 @@
+# Course
+## https://www.udemy.com/course/rest-api-flask-and-python/
+
+´´´
+This project is implemented using Flask, Flask-RESTful, Flask-SQLAlchemy and is a REST API for a store.
+´´´
+
+´´´
+Installation commands
+´´´
+    pip3 install virtualenv    
+    virtualenv venv --python=python3
+    OR 
+    python3 -m virtualenv venv --python=python3
+
+´´´
+Actvate environment
+´´´
+    (Win)
+    venv/Scripts/activate
+
+    (Linux/Mac)
+    source venv/Scripts/activate
+
+
+´´´
+Flask/Flask-RESTful installation
+´´´
+    pip3 install Flask
+    pip3 install Flask-RESTful
+
+
+´´´
+Authentication (Flask - Jason Web Token)
+´´´
+    pip3 install Flask-JWT
+
+
+´´´
+ORM
+´´´
+    pip3 install Flask-SQLAlchemy
+
+
+´´´
+List Requirements
+´´´
+    pip3 freeze
---
+++ python-flask-sqlalchemy/code/app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+from db import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'jose'
+#app.config['JWT_AUTH_URL_RULE'] = '/login'
+# config JWT to expire within half an hour
+#app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
+# config JWT auth key name to be 'email' instead of default 'username'
+#app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
+api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+jwt = JWT(app, authenticate, identity) # /login (default is /auth)
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
+
+api.add_resource(UserRegister, '/register')
+
+if __name__ == '__main__':
+    db.init_app(app)
+    app.run(port=5000, debug=True)
