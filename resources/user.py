f54fb636b544cda7b2c014ab5109f79797315713
Alberto Perdigão <alberto@PGE.CE.GOV.BR>
2022-01-13 16:53:03 -0300

    Fixed a minor error.

--- resources/user.py
+++ resources/user.py
@@ -63,4 +63,4 @@ class UserLogin(Resource):
             refresh_token = create_refresh_token(user.id)
             return {'access_token': access_token, 'refresh_token': refresh_token}, 200
         
-        return {'message', 'Invalid credentials'}, 401
+        return {'message': 'Invalid credentials'}, 401
